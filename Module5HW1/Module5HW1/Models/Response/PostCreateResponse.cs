e1edc63380696c4b7011e5aff5bd9ebb7f83c5b1
AntSenokosov <a.y.senokosov@gmail.com>
2022-02-07 20:36:08 +0200

    PostCreateResponse

---
+++ Module5HW1/Module5HW1/Models/Response/PostCreateResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Module5HW1.Models.Response;
+
+public class PostCreateResponse
+{
+    [JsonProperty("id")]
+    public string Id { get; set; } = default!;
+    [JsonProperty("name")]
+    public string Name { get; set; } = default!;
+    [JsonProperty("job")]
+    public string Job { get; set; } = default!;
+    [JsonProperty("createdAt")]
+    public string CreatedAt { get; set; } = default!;
+}
