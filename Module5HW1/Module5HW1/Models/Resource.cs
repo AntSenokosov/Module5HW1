f81f3d6ac2b70a106923cb25a904d93c056982ae
AntSenokosov <a.y.senokosov@gmail.com>
2022-02-07 19:46:52 +0200

    Add DTO Resource

---
+++ Module5HW1/Module5HW1/Models/Resource.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Module5HW1.Models;
+
+public class Resource
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+    [JsonProperty("name")]
+    public string Name { get; set; } = default!;
+    [JsonProperty("year")]
+    public int Year { get; set; }
+    [JsonProperty("color")]
+    public string Color { get; set; } = default!;
+    [JsonProperty("pantone_value")]
+    public string PantoneValue { get; set; } = default!;
+}
