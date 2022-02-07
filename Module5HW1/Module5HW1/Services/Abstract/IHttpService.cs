ff99865499f0a531dd389f95e4a15bf20998fc47
AntSenokosov <a.y.senokosov@gmail.com>
2022-02-07 20:48:31 +0200

    Add interface IHttpService

---
+++ Module5HW1/Module5HW1/Services/Abstract/IHttpService.cs
@@ -0,0 +1,6 @@
+﻿namespace Module5HW1.Services.Abstract;
+
+public interface IHttpService
+{
+    public Task<HttpResponseMessage> SendAsync(string url, HttpMethod httpMethod, StringContent? content = null);
+}
