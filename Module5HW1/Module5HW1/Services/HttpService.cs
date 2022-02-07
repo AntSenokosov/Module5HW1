adec175a3e25a716938b7492b7e7511198847b1a
AntSenokosov <a.y.senokosov@gmail.com>
2022-02-07 20:50:49 +0200

    Add HttpService

---
+++ Module5HW1/Module5HW1/Services/HttpService.cs
@@ -0,0 +1,22 @@
+﻿using Module5HW1.Models.Response;
+using Module5HW1.Services.Abstract;
+using Newtonsoft.Json;
+
+namespace Module5HW1.Services;
+
+public class HttpService : IHttpService
+{
+    private readonly HttpClient _client = new HttpClient();
+
+    public async Task<HttpResponseMessage> SendAsync(string url, HttpMethod httpMethod, StringContent? content = null)
+    {
+        var httpMessage = new HttpRequestMessage();
+        httpMessage.Content = content;
+        httpMessage.Method = httpMethod;
+        httpMessage.RequestUri = new Uri(url);
+
+        var result = await _client.SendAsync(httpMessage);
+
+        return result;
+    }
+}
