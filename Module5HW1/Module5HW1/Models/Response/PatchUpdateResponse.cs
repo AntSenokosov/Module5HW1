565efbc2853c299be3e8d7d316a5af41abba54a7
AntSenokosov <a.y.senokosov@gmail.com>
2022-02-07 20:33:19 +0200

    Add PatchUpdateResponse

---
+++ Module5HW1/Module5HW1/Models/Response/PatchUpdateResponse.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Module5HW1.Models.Response;
+
+public class PatchUpdateResponse
+{
+    [JsonProperty("name")]
+    public string Name { get; set; } = default!;
+    [JsonProperty("job")]
+    public string Job { get; set; } = default!;
+    [JsonProperty("updatedAt")]
+    public string UpdatedAt { get; set; } = default!;
+}
